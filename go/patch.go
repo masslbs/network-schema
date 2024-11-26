4382c55569cf8a7dcd742c6572489eeaba89fc1d
Martin Becze <martin@becze.org>
2024-11-26 08:43:09 +0000

    Added some description to patch.go

--- go/patch.go
+++ go/patch.go
@@ -1,3 +1,10 @@
+/*
+https://datatracker.ietf.org/doc/html/rfc6902/
+This is a modified version of JSON Patch (rfc6902). We first constraint the operations to "add", "replace", "remove" and then we add the following operations
+- increment - Increments a number by a specified value. Only valid if the value of the path is a number.,
+- decrement - Decrements a number by a specified value. Only valid if the value of the path is a number.,
+*/
+
 package main
 
 type write struct {
@@ -5,8 +12,17 @@ type write struct {
 }
 
 type patch struct {
-	op   string
+	op   Opstring
 	path string
 	// ??
 	value map[string]interface{}
 }
+
+type OpString string
+const (
+    AddOp OpString = "add"
+    ReplaceOp OpString = "replace"
+    RemoveOp OpString = "remove"
+    IncrementOp OpString = "increment"
+    DecrementOp OpString = "decrement"
+)
